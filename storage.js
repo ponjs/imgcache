38fbb0c380c95fc4a00f0d46e0607ded745029ae
ponjs <2552658135@qq.com>
2021-03-09 14:35:34 +0800

    修复缓存赋值

--- storage.js
+++ storage.js
@@ -16,36 +16,32 @@ class Storage {
   }
   // 设置缓存
   set storage(data = []) {
-    if (data.length) {
-      uni.setStorageSync(this.name, data)
-      this.db = data
-    }
+    uni.setStorageSync(this.name, data)
+    this.db = data
     return data
   }
   // 插入数据
-  insert(...args) {
+  insert({ url, local }) {
+    if (!url || !local) return
     const data = [...this.storage]
-    args.forEach(({ url, local }) => {
-      if (!url || !local) return
-      const index = data.findIndex(ele => ele.url === url)
-      index > -1 && data.splice(index, 1)
-      data.push({ url, local })
-    })
-    this.setStorage(data)
+    const index = data.findIndex(ele => ele.url === url)
+    index > -1 && data.splice(index, 1)
+    data.push({ url, local })
+    this.storage = data
     return data
   }
   // 删除数据
   delete(obj = {}) {
     const keys = Object.keys(obj)
-    const storage = this.storage.filter(ele => !keys.some(k => obj[k] === ele[k]))
-    this.setStorage(storage)
-    return storage
+    const data = this.storage.filter(ele => !keys.some(k => obj[k] === ele[k]))
+    this.storage = data
+    return data
   }
   // 查询数据
   select(obj = {}) {
     const keys = Object.keys(obj)
-    const storage = this.storage.filter(ele => keys.every(k => obj[k] === ele[k]))
-    return storage
+    const data = this.storage.filter(ele => keys.every(k => obj[k] === ele[k]))
+    return data
   }
   // 清空数据
   clear() {
